83bd72b23932d3a66380dbbfe8a9a8cacb61efe0
Niko Chaffin <niko@bixly.com>
2016-12-27 14:35:11 -0800

    Added base angular app

---
+++ app/app.js
@@ -0,0 +1,25 @@
+(function(window) {
+  angular.module('pieco', ['ui.router']);
+
+  angular
+    .module('pieco')
+    .config(config);
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function config($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+    states = [
+      {
+        name: 'home',
+        url: '/',
+        templateUrl: 'app/home/home.view.html',
+        controller: 'homeCtrl'
+      }
+    ]
+
+    states.forEach(function(state) {
+      $stateProvider.state(state);
+    });
+  }
+})(window);
---
+++ app/main.controller.js
@@ -0,0 +1,9 @@
+(function() {
+  angular
+    .module('pieco')
+    .controller('mainCtrl', mainCtrl);
+
+  function mainCtrl() {
+    console.log("Main PieCo Demo controller loaded.");
+  }
+}());
